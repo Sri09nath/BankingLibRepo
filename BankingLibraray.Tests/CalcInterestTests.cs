bcf7ebff4c60d9d2258d31ef8048f40033d0523c
Srinath Papisetty <Srinath.Papisetty@lntinfotech.com>
2021-09-20 14:59:55 +0530

    first commit

---
+++ BankingLibraray.Tests/CalcInterestTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using BankingLibrary;
+
+namespace BankingLibraray.Tests
+{
+    [TestClass]
+    public class CalcInterestTests
+    {
+        [TestMethod]
+        public void Calculate_AcceptsPNR_ReturnsSI()
+        {
+            //Arrange
+            int ExpectedResult = 1000;
+
+            //Act
+            ICalcInterest calc = new CalcInterest();
+            int ActualResult = calc.Calculate(10000, 1, 10);
+
+            //Assert
+            Assert.AreEqual(ActualResult, ExpectedResult);
+        }
+    }
+}
---
+++ BankingLibraray.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BankingLibraray.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("BankingLibraray.Tests")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f1a5da4b-2301-4f55-9650-d94614f45b68")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
